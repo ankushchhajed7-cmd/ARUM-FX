550135bb2ec324ed6d3bc24812a9faaf83071895
ankushchhajed7-cmd <ankushchhajed7@gmail.com>
2026-09-22 20:02:45 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'aurum-fx-v1';
+const CACHE = 'aurum-fx-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
